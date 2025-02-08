5187b35f751a83f33b22c1b1d10ef56b042ac42d
obsioasisestudios <149190016+obsioasisestudios@users.noreply.github.com>
2025-02-07 21:30:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -25,26 +25,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
   darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
   });
-
-  // Ripple effect
-  document.addEventListener('click', function (e) {
-    const target = e.target;
-    if (target.classList.contains('pixel-button') || target.classList.contains('collapsible')) {
-      const ripple = document.createElement('span');
-      ripple.classList.add('ripple');
-
-      const rect = target.getBoundingClientRect();
-      const x = e.clientX - rect.left;
-      const y = e.clientY - rect.top;
-
-      ripple.style.left = `${x}px`;
-      ripple.style.top = `${y}px`;
-
-      target.appendChild(ripple);
-
-      ripple.addEventListener('animationend', function () {
-        ripple.remove();
-      });
-    }
-  });
 });
